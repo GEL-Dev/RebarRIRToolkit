316972cfbfc823f96d86d50a22c60585cc307c36
yishizu <ykishizu@gmail.com>
2024-04-15 06:15:42 +0900

    clean up

--- my_package/geometry/array_pattern.py
+++
@@ -1,8 +0,0 @@
-class ArrayPattern:
-    def create_grid_pattern(self, start_point, x_count, y_count, spacing):
-        points = []
-        for i in range(x_count):
-            for j in range(y_count):
-                point = (start_point[0] + i * spacing, start_point[1] + j * spacing, start_point[2])
-                points.append(point)
-        return points
--- my_package/utils/rebar_utils.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from Autodesk.Revit.DB.Structure import Rebar, RebarBarType, RebarShape, RebarHookType
-from Autodesk.Revit.DB import XYZ, ElementId,FilteredElementCollector, BuiltInCategory,BuiltInParameter,Transaction
-
-
-    
